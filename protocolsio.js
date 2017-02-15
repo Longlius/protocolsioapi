84c097618eb0894925a14a1ced3a9bced0f03e91
Matthew Longley <randomticktock@gmail.com>
2017-02-15 17:59:14 -0600

    first working version

--- protocolsio.js
+++ protocolsio.js
@@ -6,19 +6,83 @@ class ProtocolsIO {
 		this._apikey = apikey;
 	}
 	
-	_constructURL(keywords) {
-		var currURL = 'https://www.protocols.io/api/open/get_protocols?key=' + (this._apikey) + '&page_id=1&keywords=' + keywords;
+	_constructURL(keywords, pageid) {
+		var currURL = 'https://www.protocols.io/api/open/get_protocols?key=' + (this._apikey);
+		if (keywords != null) {
+			currURL += '&keywords=' + keywords; 
+		}
+		if (pageid != null) {
+			currURL += '&page_id' + pageid;
+		}
 		return encodeUrl(currURL);
 	}
-
-	getProtocols(keywords) {
-		var reqURL = this._constructURL(keywords);
+	
+	getProtocols(keywords, pageid, callback) {
+		var muhKeywords = (keywords == undefined) ? null : keywords;
+		var muhPageID = (pageid == undefined) ? null : pageid;
+		var reqURL = this._constructURL(muhKeywords, muhPageID);
 		request(reqURL, function(error, response, body) {
-			if (!error && response.statusCode == 200) {
-				console.log(body);
+			if (error) {
+				callback(error,null);
+				return;
+			} else {
+				switch(response.statusCode) {
+					case 200:
+						callback(null,body);
+						break;
+					default:
+						callback('UNSUPPORTED STATUS CODE: ${response.statusCode}', null);
+				}
 			}
-		}	
+		});
+	}
+	
+	getProtocolJSON(protoid, callback) {
+		var reqURL = encodeUrl('https://www.protocols.io/api/open/get_protocol_json?key=' + (this._apikey) + '&protocol_id=' + protoid);
+		request(reqURL, function(error, response, body) {
+			if(error) {
+				callback(error,null);
+				return;
+			} else {
+				switch(response.statusCode) {
+					case 200:
+						callback(null, body);
+						break;
+					default:
+						callback('UNSUPPORTED STATUS CODE: ${response.statusCode}', null);
+				}
+			}
+		});
+	}
+	
+	getProtocolPDF(protoid, callback) {
+		var reqURL = encodeUrl('https://www.protocols.io/api/open/get_protocol_pdf?key=' + (this._apikey) + '&protocol_id=' + protoid);
+		request(reqURL, function(error, response, body) {
+			if(error) {
+				callback(error,null);
+				return;
+			} else {
+				switch(response.statusCode) {
+					case 200:
+						callback(null, body);
+						break;
+					default:
+						callback('UNSUPPORTED STATUS CODE: ${response.statusCode}', null);
+				}
+			}
+		});
 	}
 }
 
-export { ProtocolsIO as default }
+var test = function() {
+	var muhApiKey = 'a133bf5894b63d3cbf0c5f9698aad215';
+	var protocols = new ProtocolsIO(muhApiKey);
+	protocols.getProtocolJSON('5038', function(error, result){
+			if(error) {
+				console.log(error);
+			} else {
+				console.log(result);
+			}
+	});
+};
+module.exports = ProtocolsIO;
